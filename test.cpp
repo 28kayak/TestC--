fa35b2fac1e1ed99c3a48a44f2f0d87d112dc91f
kaya <kaya_ota@msn.com>
2013-01-07 10:38:18 -0800

    initial commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+//accrding to a book#include "iostream.h" but can not open souce file....why?
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "the first c++ prgram\n";//ERROR: cout is undefined
+	cout << "Hello World";
+
+
+	return 0;
+}
+
